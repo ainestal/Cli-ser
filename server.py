118b1efb6482c7dd844ea67d21efabe255ddd387
ainestal <ainestal@gmail.com>
2013-09-13 15:28:05 +0200

    server updated

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import socket
 import argparse
 
@@ -20,11 +21,16 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
-conn, addr = s.accept()
-print 'Connection address:', addr
 while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print "received data:", data
-    conn.send(data)  # echo
-conn.close()
+    conn, addr = s.accept()
+    if os.fork():
+       print 'parent'
+       conn.close()
+    else:
+       print 'child'
+       data = conn.recv(BUFFER_SIZE)
+#    if not data: break
+       print "received data:", data
+       #conn.send(data)  # echo
+       conn.close()
+       exit(0)
